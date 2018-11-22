23bc144a6d717cf09f7840c1f6856c1c50bdd0aa
marceljouvenaz <44669735+marceljouvenaz@users.noreply.github.com>
2018-11-22 11:44:38 +0100

    computer maakt blanco statusscherm gekozen letters, begint met lege lijst (alle of alleen goede?)

--- galgje.php
+++ galgje.php
@@ -7,12 +7,17 @@ $woordenlijst = array(
 	"nek",
 	"elleboog",
 );
+$gekozen = '';
 
 
 //kies een woord 
 
 $galgwoord = $woordenlijst[rand(1,sizeof($woordenlijst))];
 $length = strlen($galgwoord);
-echo "het woord heeft $length letters";
+echo "het woord heeft $length letters" . PHP_EOL;
+
+//blanco statusscherm
+//gekozen letters, woordstatus, foute letters, stand van de galg
+echo "U heeft de volgende letters al gekozen: " . $gekozen . PHP_EOL;
 
 ?>
