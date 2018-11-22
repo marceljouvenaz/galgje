16322a203a96d7469f31348df04f7d0e5aa2bab4
marceljouvenaz <44669735+marceljouvenaz@users.noreply.github.com>
2018-11-22 11:57:04 +0100

    woordstatus, begint bij alleen lege plekken

--- galgje.php
+++ galgje.php
@@ -8,16 +8,16 @@ $woordenlijst = array(
 	"elleboog",
 );
 $gekozen = '';
-
+$woordstatus = array('',);
 
 //kies een woord 
-
-$galgwoord = $woordenlijst[rand(1,sizeof($woordenlijst))];
+$galgwoord = $woordenlijst[rand(1, sizeof($woordenlijst))];
 $length = strlen($galgwoord);
 echo "het woord heeft $length letters" . PHP_EOL;
 
 //blanco statusscherm
 //gekozen letters, woordstatus, foute letters, stand van de galg
 echo "U heeft de volgende letters al gekozen: " . $gekozen . PHP_EOL;
+echo "U weet dit van het woord: " . $woordstatus[0] . PHP_EOL;
 
 ?>
