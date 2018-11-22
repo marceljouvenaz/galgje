ff06951e5310461bf80b6d0b2285b69adc10ee38
marceljouvenaz <44669735+marceljouvenaz@users.noreply.github.com>
2018-11-22 13:30:31 +0100

    stand van de galg, begint met blanco plaatje

--- galgje.php
+++ galgje.php
@@ -20,4 +20,14 @@ echo "het woord heeft $length letters" . PHP_EOL;
 echo "U heeft de volgende letters al gekozen: " . $gekozen . PHP_EOL;
 echo "U weet dit van het woord: " . $woordstatus[0] . PHP_EOL;
 echo "De volgende letters staan niet in het woord: " . PHP_EOL;
+//blanco galg
+echo (" +---+") . PHP_EOL;
+echo (" |   |") . PHP_EOL;
+echo ("     |") . PHP_EOL;
+echo ("     |") . PHP_EOL;
+echo ("     |") . PHP_EOL;
+echo ("     |") . PHP_EOL;
+echo ("     |") . PHP_EOL;
+echo ("=======") . PHP_EOL;
+
 ?>
