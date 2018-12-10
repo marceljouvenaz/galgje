c7949e7bc14910597fccb21aa680cb39385a3420
marceljouvenaz <44669735+marceljouvenaz@users.noreply.github.com>
2018-12-10 14:26:21 +0100

    tekent galgjes

--- galgje.php
+++ galgje.php
@@ -9,6 +9,90 @@ $woordenlijst = array(
 );
 $gekozen = '';
 $woordstatus = array('',);
+$aantalfouten = 0;
+
+//function
+
+function tekengalg($arg1)
+{
+	if ($arg1 == 0) {
+		//blanco galg
+		echo (" +---+") . PHP_EOL;
+		echo (" |   |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("=======") . PHP_EOL;
+	}
+	if ($arg1 == 1) {
+		// galg1 
+		echo (" +---+") . PHP_EOL;
+		echo (" |   |") . PHP_EOL;
+		echo (" o   |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("=======") . PHP_EOL;	
+	}
+	if ($arg1 == 2) {
+		//galg2
+		echo (" +---+") . PHP_EOL;
+		echo (" |   |") . PHP_EOL;
+		echo (" o   |") . PHP_EOL;
+		echo (" |   |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("=======") . PHP_EOL;
+	}
+	if ($arg1 == 3){
+		//galg3
+		echo (" +---+") . PHP_EOL;
+		echo (" |   |") . PHP_EOL;
+		echo (" o   |") . PHP_EOL;
+		echo ("/|   |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("=======") . PHP_EOL;
+	}
+	if ($arg1 == 4){
+		//galg4
+		echo (" +---+") . PHP_EOL;
+		echo (" |   |") . PHP_EOL;
+		echo (" o   |") . PHP_EOL;
+		echo ("/|\  |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("=======") . PHP_EOL;
+	}
+	if ($arg1 == 5){
+		//galg5
+		echo (" +---+") . PHP_EOL;
+		echo (" |   |") . PHP_EOL;
+		echo (" o   |") . PHP_EOL;
+		echo ("/|\  |") . PHP_EOL;
+		echo ("/    |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("=======") . PHP_EOL;
+	}
+	if ($arg1 == 6){
+		//galg6
+		echo (" +---+") . PHP_EOL;
+		echo (" |   |") . PHP_EOL;
+		echo (" o   |") . PHP_EOL;
+		echo ("/|\  |") . PHP_EOL;
+		echo ("/ \  |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("     |") . PHP_EOL;
+		echo ("=======") . PHP_EOL;
+	}
+}
 
 //kies een woord 
 $galgwoord = $woordenlijst[rand(1, sizeof($woordenlijst)) - 1];
@@ -16,20 +100,12 @@ $length = strlen($galgwoord);
 echo "het woord heeft $length letters" . PHP_EOL;
 
 //blanco statusscherm
+tekengalg($aantalfouten);
+
 //gekozen letters, woordstatus, foute letters, stand van de galg
 echo "U heeft de volgende letters al gekozen: " . $gekozen . PHP_EOL;
 echo "U weet dit van het woord: " . $woordstatus[0] . PHP_EOL;
 echo "De volgende letters staan niet in het woord: " . PHP_EOL;
-//blanco galg
-echo (" +---+") . PHP_EOL;
-echo (" |   |") . PHP_EOL;
-echo ("     |") . PHP_EOL;
-echo ("     |") . PHP_EOL;
-echo ("     |") . PHP_EOL;
-echo ("     |") . PHP_EOL;
-echo ("     |") . PHP_EOL;
-echo ("=======") . PHP_EOL;
-
 //deel 3, computer vraagt een letter,
 
 echo "Geef uw keuze voor een letter." . PHP_EOL;
@@ -45,11 +121,16 @@ if (ctype_alpha($input)) {
 		} 
 		else {
 			echo "U heeft deze letter niet eerder gekozen" . PHP_EOL;
+			$gekozen .= $input;
 			if (is_numeric(strpos($galgwoord, $input))) {
-				echo "goedzo, deze letter komt voor in het woord." . PHP_EOL
-				;
+				echo "goedzo, deze letter komt voor in het woord." . PHP_EOL;
+				// toon woord met alle bekende letters
+
 			} else {
-				echo "jammer, deze letter komt niet voor in het woord.";
+				echo "jammer, deze letter komt niet voor in het woord." . PHP_EOL;
+				$aantalfouten += 1;
+				tekengalg($aantalfouten);
+				// nieuw plaatje
 			}	
 		}
 		$gekozen .=  $input;
